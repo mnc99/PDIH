42f5020500913c40e8a14d1f4fbf15fbfd475f57
mnc99 <moises.noguera1999@gmail.com>
2022-03-28 21:13:47 +0200

    Añadida función para mover pelota

--- P2/pong.py
+++ P2/pong.py
@@ -1,5 +1,6 @@
 # Pong game written in python using curses library
 import curses
+import time
 
 def create_window(nrows, ncols, posY, posX, title):
     win = curses.newwin(nrows,ncols,posY,posX)
@@ -10,11 +11,39 @@ def create_window(nrows, ncols, posY, posX, title):
 
     return win
 
+def move_ball(win):
+    maxX = curses.LINES
+    #maxY = curses.COLS
+    x = 1
+    y = 1
+    nextX = 0
+    direction = 1
+
+    curses.noecho()
+    curses.curs_set(False)
+
+    while True:
+        win.clear()
+        win.addch(y,x,'o')
+        win.refresh()
+
+        time.sleep(0.05)
+
+        nextX = x + direction
+
+        if (nextX >= maxX or nextX == 0):
+            direction *= -1
+        else:
+            x += direction
+
+
+
+
 def main(stdscr):
     
-    win = create_window(20,20,10,10,'Window!')
-    win.addstr("Hello World!")
-    win.getch()
+    win = create_window(curses.LINES,curses.COLS,10,10,'Pong')
+    move_ball(win)
+    #win.getch()
 
 # Python permite inicializar automaticamente los recursos de ncurses
 # gracias a la funcion wrapper.
