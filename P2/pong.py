dc99de8d9b1641dd9b9c0cbb8a2989ff868b8a71
mnc99 <moises.noguera1999@gmail.com>
2022-04-01 12:26:42 +0200

    Añadida división de campo, puntos y mov barras

--- P2/pong.py
+++ P2/pong.py
@@ -13,40 +13,63 @@ def create_window(nrows, ncols, posY, posX, title):
 
 def move_ball(stdscr):
     maxY, maxX = stdscr.getmaxyx()
-    posXBar1 = x = 1
-    y = 1
+    posXBar1 = xBall = 1
+    yBall = 1
     nextX = 0
     nextY = 0
     directionx = 1
     directiony = 1
-    posYBar1 = int(round(maxY/2))
+    posYBar1 = posYBar2 = int(round(maxY/2))
+    k = 0
+    halfX = int(round(maxX/2))
+    pointsPl1 = pointsPl2 = 0
 
     curses.noecho()
-    curses.curs_set(False)
+    curses.curs_set(0)
+    stdscr.nodelay(1)
 
-    while (1):
+    while (k != ord('q')):
         stdscr.clear()
-        stdscr.addch(y,x,'o')
+
+        # Dividir la pantalla por la mitad
+        for i in range(maxY):
+            stdscr.addstr(i,halfX, '|')
+
+        stdscr.addch(yBall,xBall,'o')
         stdscr.addstr(posYBar1, 0, '|')
-        stdscr.addstr(posYBar1, maxX-1, '|')
+        stdscr.addstr(posYBar2, maxX-1, '|')
+        stdscr.addstr(1, halfX-3, str(pointsPl1))
+        stdscr.addstr(1, halfX+3, str(pointsPl2))
         stdscr.refresh()
 
         time.sleep(0.05)
 
-        nextX = x + directionx
-        nextY = y + directiony
+        nextX = xBall + directionx
+        nextY = yBall + directiony
 
         if (nextX >= maxX or nextX < 0):
             directionx *= -1
         else:
-            x += directionx
+            xBall += directionx
 
         if (nextY >= maxY or nextY < 0):
             directiony *= -1
         else:
-            y += directiony
+            yBall += directiony
 
+        k = stdscr.getch()
 
+        #Controls for left bar
+        if (k == ord('a')):
+            posYBar1 -= 1 if (posYBar1-1) > 0 else posYBar1
+        elif (k == ord('z')):
+            posYBar1 += 1 if (posYBar1+1) < maxY else posYBar1
+
+        #Controls for right bar
+        if (k == ord('k')):
+            posYBar2 -= 1 if (posYBar2-1) > 0 else posYBar2
+        elif (k == ord('m')):
+            posYBar2 += 1 if (posYBar2+1) < maxY else posYBar2
 
 
 def main(stdscr):
